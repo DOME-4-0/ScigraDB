0bc73647086122de946af8c3ecf6afcca6d79184
Adham Hashibon <78446902+adhamhashibon@users.noreply.github.com>
2024-06-27 15:41:05 +0100

    adding setup and readme commit

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A set of Tools to handle Scientific Graphs, mainly in materials science . 
 
 
+## Installation
+
+To install this package, use the following command:
+
+```bash
+pip install git+https://github.com:DOME-4-0/ScigraDB.git 
+```
--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='scigraDB',
+    version='0.1',
+    py_modules=['scigraDB'],
+    install_requires=[
+    ],
+    url='https://github.com/DOME-4-0/ScigraDB',
+    author='Adham Hashibon',
+    author_email='a.hashibon@ucl.ac.uk',
+    description='Scientific Graph Data Base Utility, replacing SimPhoNy',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
 
